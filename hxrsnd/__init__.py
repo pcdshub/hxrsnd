34be7be86547dd0b024f22d2a2981d62e5a41cf0
Zachary Lentz <zlentz@slac.stanford.edu>
2018-06-12 15:57:52 -0500

    FIX: hotfix for ophyd bug in 1.2.0

--- hxrsnd/__init__.py
+++ hxrsnd/__init__.py
@@ -1,6 +1,14 @@
 import logging
 from .plans.alignment import rocking_curve, maximize_lorentz
 
+# Hacky ophyd hotfix
+from ophyd.device import Device
+def __contains__(self, value):
+    return value in self._OphydAttrList__internal_list()
+Device.OphydAttrList.__contains__ = __contains__
+del Device
+del __contains__
+
 from ._version import get_versions
 __version__ = get_versions()['version']
 del get_versions
