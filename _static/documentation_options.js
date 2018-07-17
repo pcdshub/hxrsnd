d95485018f3bcc4a0a44b11e7eba4aeeab21ba6d
Travis CI User <travis@example.org>
2018-07-17 02:18:42 +0000

    Update docs after building Travis build 171 of pcdshub/hxrsnd
    The docs were built from the branch 'master' against the commit
    0f7f51e468440c675a9aae14ff1ff0009809df39.
    
    The Travis build that generated this commit is at
    https://travis-ci.org/pcdshub/hxrsnd/jobs/404699763.
    
    The doctr command that was run is
    
        /home/travis/miniconda/envs/test-environment/bin/doctr deploy . --built-docs docs/build/html --deploy-branch-name gh-pages

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.1+2.g5bd445c',
+    VERSION: '0.1.2+4.g0f7f51e',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
