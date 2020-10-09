ec155f4a8c798c36d957eaf4335c3a516bb6394c
Doctr (Travis CI) <drdoctr@users.noreply.github.com>
2020-10-09 21:55:06 +0000

    Update docs after building Travis build 203 of pcdshub/hxrsnd
    The docs were built from the branch 'master' against the commit
    9b05312df491903ec9cc9e7bc14a11b7c4984112.
    
    The Travis build that generated this commit is at
    https://travis-ci.com/pcdshub/hxrsnd/jobs/397603580.
    
    The doctr command that was run is
    
        /home/travis/miniconda/envs/test-environment/bin/doctr deploy . --built-docs docs/build/html --deploy-branch-name gh-pages

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.2.0+3.g634f5f7',
+    VERSION: '0.2.0+4.g9b05312',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
