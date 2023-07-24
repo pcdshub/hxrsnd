d842387420d792bf8406dbb8adc33632282253e7
Ken Lauer <klauer@slac.stanford.edu>
2023-07-24 09:35:05 -0700

    BLD: migrate to setuptools-scm
    Performed by pcds-migration-tools SetuptoolsScmMigration

--- hxrsnd/__init__.py
+++ hxrsnd/__init__.py
@@ -1,7 +1,5 @@
-from ._version import get_versions
+from .version import __version__  # noqa: F401
 from .plans.alignment import maximize_lorentz, rocking_curve
 
-__version__ = get_versions()['version']
-del get_versions
 
 __all__ = ['maximize_lorentz', 'rocking_curve']
